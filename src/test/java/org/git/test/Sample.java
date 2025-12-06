6b53cb0be08e568384d914bca017dd20b335a593
lakshmikcm <lakshmi.kcm@gmail.com>
2025-12-06 17:22:49 +0530

    Update code-Karen

---
+++ src/test/java/org/git/test/Sample.java
@@ -0,0 +1,39 @@
+package org.git.test;
+
+import org.baseclass.BaseClass;
+import org.pom.LoginPage;
+import org.pom.Registration;
+
+public class Sample extends Registration{
+	
+	public static void main(String[] args) {
+		
+		browserLaunch("chrome");
+		implicitWait(20);
+		urlLaunch("https://www.facebook.com/");
+		
+		// create object for loginpage
+		LoginPage l=new LoginPage();
+		//l.login();
+		
+		sendKeys(l.getUsername(),"Beulah");
+		sendKeys(l.getPass(),"12364");
+		//click(l.getLogin());
+		
+		Registration r=new Registration();
+		
+		click(r.getCreateAccount());
+		sendKeys(r.getFirstname(),"Leela");
+		sendKeys(r.getLastname(),"Kalai");
+		selectByIndex(r.getDay(),4);
+		selectByVisibleText(r.getMonth(),"Nov");
+		selectByVisibleText(r.getYear(),"1986");
+		click(r.getGender());
+		sendKeys(r.getMobnum(),"8908765489");
+		sendKeys(r.getNewpass(),"345678");
+		click(r.getSubmit());
+		
+	
+	}
+
+}
